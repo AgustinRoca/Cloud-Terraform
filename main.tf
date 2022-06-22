a8ae603adfd92db74b28edab138a2c63098739b0
luciatorrusio <lucia.torrusio@gmail.com>
2022-06-22 11:41:09 -0300

    added information to provider and editors

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ terraform {
 provider "google" {
   project = var.project
   region  = var.region
+  zone = "us-central1-c"
+  # este lo sacamos de cuando creamos el proyecto
+  # hay que hacer un service account en create key elegir json
+  credentials = "${file("service-acount.json")}"
 }
 
 # Create a new project under the billing account
@@ -24,6 +28,22 @@ resource "google_project_iam_member" "owner" {
   member  = "user:aroca@itba.edu.ar"
   depends_on = [google_project.project]
   project = google_project.project.project_id
+}
+
+resource "google_project_iam_binding" "project" {
+  project = var.project
+  role    = "roles/editor"
+
+  members = [
+    "user:ltorrusio@itba.edu.ar",
+    "user:lucidiaz@itba.edu.ar"
+  ]
+
+  # condition {
+  #   title       = "expires_after_2019_12_31"
+  #   description = "Expiring at midnight of 2019-12-31"
+  #   expression  = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
+  # }
 }
 
 # Enable APIs
