c71adad87f0b184ff3ccd194e242df456f596a1d
luciatorrusio <lucia.torrusio@gmail.com>
2022-07-01 14:58:45 -0300

    better email

--- terraform-gcp/resources/warning_trigger_scripts/main.py
+++ terraform-gcp/resources/warning_trigger_scripts/main.py
@@ -39,14 +39,18 @@ def sendWarningMail(arg1, arg2):
     <body>
         <p>Hi,<br>
         <br>
-        This is to inform that an image has been added to your bucket on this date: {}, arg1:  {}, arg2: {} <br>
+        New event:
+        Date:{}
+        Bucket: {}
+        Object added: {}
+        <br>
         Thank you. <br>
         </p>
     </body>
     </html>
     """
 
-    body_html = MIMEText(html.format(today_date, arg1, arg2), 'html')  # parse values into html text
+    body_html = MIMEText(html.format(today_date, arg1.bucket, arg2.resource.name), 'html')  # parse values into html text
     msg.attach(body_html)  # attaching the text body into msg
 
     ## Image
